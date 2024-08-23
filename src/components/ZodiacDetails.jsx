281434872d1c1289a7a3dbc9342bc928529b6709
VadimLitau <vadimlitau@yandex.ru>
2024-08-23 22:25:03 +0500

    swipe edit

--- src/components/ZodiacDetails.jsx
+++ src/components/ZodiacDetails.jsx
@@ -2,20 +2,40 @@
 import { useEffect } from 'react';
 function ZodiacDetails({ horoscope, onBack, p }) {
     useEffect(() => {
-        // Активируем кнопку "Назад"
         window.Telegram.WebApp.BackButton.show();
 
-        // Назначаем обработчик события на нажатие кнопки "Назад"
-        window.Telegram.WebApp.BackButton.onClick(() => {
-            onBack(); // Закрываем окно описания и возвращаемся к основному списку
-        });
+        window.Telegram.WebApp.BackButton.onClick(onBack);
 
-        // Чистим обработчик и скрываем кнопку "Назад" при размонтировании компонента
+        let touchStartX = 0;
+        let touchEndX = 0;
+
+        const handleTouchStart = (e) => {
+            touchStartX = e.changedTouches[0].screenX;
+        };
+
+        const handleTouchMove = (e) => {
+            touchEndX = e.changedTouches[0].screenX;
+        };
+
+        const handleTouchEnd = () => {
+            if (touchEndX > touchStartX + 50) {
+                onBack();
+            }
+        };
+
+        window.addEventListener('touchstart', handleTouchStart);
+        window.addEventListener('touchmove', handleTouchMove);
+        window.addEventListener('touchend', handleTouchEnd);
+
         return () => {
             window.Telegram.WebApp.BackButton.hide();
             window.Telegram.WebApp.BackButton.offClick(onBack);
+            window.removeEventListener('touchstart', handleTouchStart);
+            window.removeEventListener('touchmove', handleTouchMove);
+            window.removeEventListener('touchend', handleTouchEnd);
         };
-    }, []);
+    }, [onBack]);
+
     return (
         <div className="zodiac-details">
             <button onClick={onBack}>Назад</button>
