eae568843e6a4d1661adeef20052a009128ddf9f
VadimLitau <vadimlitau@yandex.ru>
2024-08-23 22:23:16 +0500

    tg back button

--- src/components/ZodiacDetails.jsx
+++ src/components/ZodiacDetails.jsx
@@ -1,5 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react';
 function ZodiacDetails({ horoscope, onBack, p }) {
+    useEffect(() => {
+        // Активируем кнопку "Назад"
+        window.Telegram.WebApp.BackButton.show();
+
+        // Назначаем обработчик события на нажатие кнопки "Назад"
+        window.Telegram.WebApp.BackButton.onClick(() => {
+            onBack(); // Закрываем окно описания и возвращаемся к основному списку
+        });
+
+        // Чистим обработчик и скрываем кнопку "Назад" при размонтировании компонента
+        return () => {
+            window.Telegram.WebApp.BackButton.hide();
+            window.Telegram.WebApp.BackButton.offClick(onBack);
+        };
+    }, []);
     return (
         <div className="zodiac-details">
             <button onClick={onBack}>Назад</button>
